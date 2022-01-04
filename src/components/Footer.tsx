bcc619922c10db5b9a937a9c3469e2762c7f6a28
CesarArellano <raywayday@gmail.com>
2022-01-03 20:27:02 -0600

    Updating text and removing imports

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
 export const Footer = () => {
   return (
     <div className="copyright">
-      <p>Copyright &copy; 2021 César Arellano. All Right Reserved</p>
+      <p>Copyright &copy; 2022 César Arellano. All Right Reserved</p>
     </div>
   )
 }
