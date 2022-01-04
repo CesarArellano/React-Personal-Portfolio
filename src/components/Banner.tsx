fc88065cbc87674763a4d7e22a5f1c3ac2e60298
CesarArellano <raywayday@gmail.com>
2022-01-03 19:47:52 -0600

    Adding new Build

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,7 +5,7 @@ export const Banner = () => {
         <div className="textBx animate__animated animate__fadeInLeft">
           <h2>Hello, I'm <br /><span>César Arellano</span></h2>
           <h3>I'm a Front-End Web and Mobile Developer</h3>
-          <a href="/#about" className="btn hoverable">About Me</a>
+          <a href="#about" className="btn hoverable">About Me</a>
         </div>
       </section>
     </>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,15 +23,15 @@ export const Navbar = () => {
 
   return (
     <header className={ (isModeSticky ) ? 'sticky' : '' }>
-      <a href="/#" className="logo"> Portfolio </a>
+      <a href="#" className="logo"> Portfolio </a>
       <div className="toggle" onClick={ toggleMenu }></div>
       <ul className={ `menu ${ isActiveNavbar ? 'active' : '' }`}>
-        <li><a href="/#home" onClick={ toggleMenu }> Home </a></li>
-        <li><a href="/#about" onClick={ toggleMenu }> About </a></li>
-        <li><a href="/#services" onClick={ toggleMenu }> Services </a></li>
-        <li><a href="/#work" onClick={ toggleMenu }> Work </a></li>
-        <li><a href="/#certifications" onClick={ toggleMenu }> Certifications </a></li>
-        <li><a href="/#contact" onClick={ toggleMenu }> Contact </a></li>
+        <li><a href="#home" onClick={ toggleMenu }> Home </a></li>
+        <li><a href="#about" onClick={ toggleMenu }> About </a></li>
+        <li><a href="#services" onClick={ toggleMenu }> Services </a></li>
+        <li><a href="#work" onClick={ toggleMenu }> Work </a></li>
+        <li><a href="#certifications" onClick={ toggleMenu }> Certifications </a></li>
+        <li><a href="#contact" onClick={ toggleMenu }> Contact </a></li>
       </ul>
     </header>
   )
